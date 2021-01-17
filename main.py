7f8614a6c1139db8de771a828ea3af1d91c3ffe6
maciekbaczmanski <cakeisalie16@gmail.com>
2021-01-17 05:37:54 +0100

    initial

--- main.py
+++ main.py
@@ -4,12 +4,60 @@ import player
 
 p1 = player.Player(1)
 p2 = player.Player(2)
+starttime = None
+ingame = False
+
+
+def startgame():
+    global ingame, client, starttime
+    ingame = True
+    client.publish("game/ready", 0)
+    starttime = time.time()
+
+
+def finishgame():
+    global p1, p2, ingame
+    if p1.points > p2.points:
+        client.publish("game/finished", 1)
+    elif p1.points < p2.points:
+        client.publish("game/finished", 2)
+    elif p1.points == p2.points:
+        client.publish("game/finished", 0)
+    p1.clear()
+    p2.clear()
+    ingame = False
 
 
 def on_message(client, userdata, message):
-    global p1, p2
+    global p1, p2, ingame, starttime
     if message.topic == "player1/ready":
-        pass
+        p1.ready()
+        if p2.ifready:
+            startgame()
+    elif message.topic == "player2/ready":
+        p2.ready()
+        if p1.ifready:
+            startgame()
+    elif message.topic == "player1/quit":
+        p1.quit()
+        if ingame:
+            finishgame()
+    elif message.topic == "player2/quit":
+        p2.quit()
+        if ingame:
+            finishgame()
+
+    elif message.topic == "Akcelerometr/Down" and ingame:
+        p1.addpoint()
+        client.publish("player1/points", p1.points)
+        if abs(starttime - time.time()) > 20:
+            finishgame()
+    elif message.topic == "AkcelerometrBur/Down" and ingame:
+        p2.addpoint()
+        client.publish("player2/points", p2.points)
+        if abs(starttime - time.time()) > 20:
+            finishgame()
+
 
 ########################################
 
--- player.py
+++ player.py
@@ -7,9 +7,12 @@ class Player:
     def addpoint(self):
         self.points += 1
 
-
     def ready(self):
         self.ifready = True
 
     def quit(self):
+        self.ifready = False
+
+    def clear(self):
+        self.points = 0
         self.ifready = False
